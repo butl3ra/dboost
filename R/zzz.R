53ee1a2ca80d16ec64f6c43a71718bb67ac9baca
butl3ra <andrew.butler.d@gmail.com>
2022-04-25 12:13:13 -0400

    only source the class on load

--- R/zzz.R
+++ R/zzz.R
@@ -23,11 +23,12 @@ assign("elapsedTime", 0, envir = .dboostEnv)
   mb = microbenchmark::microbenchmark
 
   # --- source defined classes: inst
-  spor_inst_root = system.file("",package = 'dboost')
-  folders = list.files(spor_inst_root,full.names=T)
+  inst_root = system.file("",package = 'dboost')
+  folders = list.files(inst_root,full.names=T)
   idx = grepl("inst__",folders)
   folders = folders[idx]
   files = list.files(folders,full.names=T)
+  files = files[grepl('class',files)]
   for(tmp in files){
     source(tmp)
   }
